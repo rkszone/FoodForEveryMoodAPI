25f9a1f9fd04d0711e18ee9687d00a9c2442be1f
Rahul Kumar Singh <rahul.kumar.singh@nl.abnamro.com>
2021-04-03 18:09:49 +0200

    initial set up

---
+++ src/main/java/com/ffem/api/FoodForEveryMoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.ffem.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodForEveryMoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodForEveryMoodApiApplication.class, args);
+	}
+
+}
